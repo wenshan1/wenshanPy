71a6767c9ad975f684ecd71d23d0fcae20ef4115
Bin Lan <Bin.Lan@windriver.com>
2021-03-26 11:20:00 +0800

    添加字符是否有非ascii

---
+++ windriver/vx7-sc0630/fileIsAscii.py
@@ -0,0 +1,25 @@
+# fileIsAscii.py
+
+def scanFileForOnlyAscii(fn):
+    badLines = []
+    with open(fn, "rt") as f:
+        if hasattr(str, 'isascii'):
+            badLines = [(linenum, line) for linenum, line in enumerate(f.readlines()) if not line.isascii()]
+        else:
+            for linenum, line in enumerate(f.readlines()):
+                if not all(ord(char) < 128 for char in line):
+                    badLines.append((linenum, line))
+
+    return badLines
+
+if __name__ == '__main__':
+    import sys
+    for fn in sys.argv[1:]:
+        nonAsciiLines = scanFileForOnlyAscii(fn)
+
+        print()
+        print(fn)
+        for lnum, line in nonAsciiLines:
+            print(f"{lnum:5} - {line}", end='')
+            print(f"{' ':5} - ", end='')
+            print(''.join([' ' if c.isascii() else '^' for c in line]))
